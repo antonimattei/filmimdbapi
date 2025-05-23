ff42788db8b958a33f556ba36a41788e171d774c
antonimattei <antonidemedeiros@gmail.com>
2025-05-23 14:24:17 -0300

    Ajustado o campo ano que não trazia e implementado tambem fastAPI com paginação

--- controllers/movie_controller.py
+++ controllers/movie_controller.py
@@ -1,15 +1,19 @@
 from flask import Blueprint, render_template, request
-from models.movie_model import search_movies, get_movie_by_id
+from models.movie_model import search_movies
 
 movie_bp = Blueprint('movie_bp', __name__)
 
 @movie_bp.route('/')
 def index():
     title = request.args.get('title')
-    movies = search_movies(title) if title else []
-    return render_template('index.html', movies=movies, search_title=title)
-
-@movie_bp.route('/movie/<movie_id>')
-def movie_details(movie_id):
-    movie = get_movie_by_id(movie_id)
-    return render_template('movie_details.html', movie=movie)
+    page = int(request.args.get('page', 1))
+    
+    if title:
+        result = search_movies(title, page=page)
+        return render_template(
+            'index.html',
+            movies=result["movies"],
+            pagination=result["pagination"],
+            search_title=title
+        )
+    return render_template('index.html', movies=[], search_title=None)
