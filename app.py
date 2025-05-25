d3547969dd43a1188b8604129f7e4245f6b7290b
antonimattei <antonidemedeiros@gmail.com>
2025-05-25 17:51:51 -0300

    correção

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from controllers.movie_controller import movie_bp
 import os
 
 app = Flask(__name__)
 
 # Registre o Blueprint (roteamento de filmes)
-app.register_blueprint(movie_bp, url_prefix='/')  # Ex: /movies/list
+app.register_blueprint(movie_bp, url_prefix='/movies')  # Ex: /movies/list
 
 @app.route("/")
 def home():
-    return "Página inicial do site!"
+    return redirect(url_for('movie_bp.index'))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 1000)), debug=False)
