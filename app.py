43bfe987320ebbb8e42b71b04fa097ba2f710c81
antonimattei <antonidemedeiros@gmail.com>
2025-05-25 22:02:01 -0300

    ok

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask, render_template
-from controllers.movie_controller import movie_bp  # Importe o Blueprint
+from flask import Flask
+from controllers.movie_controller import movie_bp, index  # Importe index
 import os
 
 app = Flask(__name__)
 
-# Registre o Blueprint (roteamento de filmes)
-app.register_blueprint(movie_bp, url_prefix='/movies')  # Ex: /movies/list
+app.register_blueprint(movie_bp, url_prefix='/movies')
 
-@app.route("/")
-def home():
-     return render_template("index.html")
+# Use a função index do movie_controller para a rota /
+app.add_url_rule("/", view_func=index)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 1000)), debug=False)
