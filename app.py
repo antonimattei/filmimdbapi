d1d5038c8cc9f13ff96d52ca1677ec69d9c33103
antonimattei <antonidemedeiros@gmail.com>
2025-05-25 22:32:07 -0300

    ajustes finais

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from controllers.movie_controller import movie_bp, index  # Importe index
+from controllers.movie_controller import movie_bp, index
 import os
 
 app = Flask(__name__)
 
-app.register_blueprint(movie_bp, url_prefix='/movies')
+app.register_blueprint(movie_bp)  # Remova o url_prefix
 
-# Use a função index do movie_controller para a rota /
-app.add_url_rule("/movies", view_func=index)
+# Corrija a rota principal para "/"
+app.add_url_rule("/", view_func=index)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 1000)), debug=False)
